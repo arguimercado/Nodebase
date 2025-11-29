b432a46af6180a02c3f3df173e6bdcf336000c31
amercado <arnold.mercado@hotmail.com>
2025-11-29 12:11:29 +0800

    Workflow Form

--- src/app/(root)/(others)/workflows/_features/components/workflow-list.tsx
+++ src/app/(root)/(others)/workflows/_features/components/workflow-list.tsx
@@ -14,6 +14,7 @@ import {
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { WorkflowIcon } from "lucide-react";
+import WorkflowMenu from "./workflow-menu";
 
 const WorkflowList = () => {
 
@@ -65,6 +66,14 @@ const WorkflowList = () => {
         <p>{new Date(row.original.updatedAt).toLocaleDateString()}</p>
       ),
     },
+    {
+      id: "actions",
+      header: () => <HeaderColumn alignment="right">Actions</HeaderColumn>,
+      cell: ({ row }) => {
+        const id = row.original.id;
+        return (<WorkflowMenu id={id} />);
+      }
+    }
   ];
 
   
